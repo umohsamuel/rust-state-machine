7c6063a5c3497880923e32b0a092940661d6d9af
Umoh Samuel Godwin <umohsg@gmail.com>
2025-02-05 01:04:27 +0100

    completing basic balance pallet

--- src/balances.rs
+++ src/balances.rs
@@ -16,4 +16,69 @@ impl Pallet {
 	pub fn balance(&self, who: &String) -> u128 {
 		*self.balances.get(who).unwrap_or(&0)
 	}
+
+	pub fn transfer(&mut self, caller: String, to: String, amount: u128)-> Result<(), &'static str>{
+		/* TODO:
+			- Get the balance of account `caller`.
+			- Get the balance of account `to`.
+
+			- Use safe math to calculate a `new_caller_balance`.
+			- Use safe math to calculate a `new_to_balance`.
+
+			- Insert the new balance of `caller`.
+			- Insert the new balance of `to`.
+		*/
+
+		let caller_balance = self.balance(&caller);
+
+		let to_balance = self.balance(&to);
+
+		let new_caller_balance = caller_balance.checked_sub(amount).ok_or("Insufficient balance")?;
+		let new_to_balance = to_balance.checked_add(amount).ok_or("Failed to complete")?;
+
+		self.set_balance(&caller, new_caller_balance);
+		self.set_balance(&to, new_to_balance);
+
+		Ok(())
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::Pallet;
+
+	#[test]
+	fn init_balances() {
+		/* TODO: Create a mutable variable `balances`, which is a new instance of `Pallet`. */
+		/* TODO: Assert that the balance of `alice` starts at zero. */
+		/* TODO: Set the balance of `alice` to 100. */
+		/* TODO: Assert the balance of `alice` is now 100. */
+		/* TODO: Assert the balance of `bob` has not changed and is 0. */
+
+		let mut balances = super::Pallet::new();
+		assert_eq!(balances.balance(&"alice".to_string()), 0);
+		balances.set_balance(&"alice".to_string(), 100);
+		assert_eq!(balances.balance(&"alice".to_string()), 100);
+		assert_eq!(balances.balance(&"bob".to_string()), 0);
+	}
+
+	#[test]
+	fn transfer_balance() {
+		/* TODO: Create a test that checks the following:
+			- That `alice` cannot transfer funds she does not have.
+			- That `alice` can successfully transfer funds to `bob`.
+			- That the balance of `alice` and `bob` is correctly updated.
+		*/
+		let mut balances = super::Pallet::new();
+
+		assert_eq!(balances.transfer("alice".to_string(), "bob".to_string(), 100), Err("Insufficient balance"));
+
+		balances.set_balance(&"alice".to_string(), 100);
+
+		assert_eq!(balances.transfer("alice".to_string(), "bob".to_string(), 50), Ok(()));
+
+		assert_eq!(balances.balance(&"alice".to_string()), 50);
+		assert_eq!(balances.balance(&"bob".to_string()), 50);
+
+	}
 }
